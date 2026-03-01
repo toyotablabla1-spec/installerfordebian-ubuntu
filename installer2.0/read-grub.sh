d49e425848e40fb7f0c25ab232d968347646dc07
TOYOTA <toyotablabla1@gmail.com>
2026-03-01 08:34:06 +0200

    v2

---
+++ installer2.0/read-grub.sh
@@ -0,0 +1,30 @@
+echo "
+  ______║║ ┃┃  ┃┃┃ ┃    ┃  ┃            ___  ___
+ ╱ ╱__╲ ║║ ┃┃┃┃┃   ┃    ┃  ┃            |_|  |_|
+ | ║  ║ ║║ ┃┃      ┃    ┃  ┃┃┃┃┃        ________   _|_
+ | ╲__╱ ║║ ┃┃      ┃    ┃  ┃   ┃┃       \______/    |
+ ╲______║║ ┃┃      ┃    ┃  ┃    ┃
+        ║║ ┃┃      ┃    ┃  ┃    ┃
+        ╱╱ ┃┃      ┃    ┃  ┃   ┃┃
+  ══════┃┃ ┃┃      ┃┃┃┃┃┃  ┃┃┃┃┃   THEMES
+  BY SUPERFORCE                chose grub uncomment and comment old chosed grub      Edit read-grub.sh
+  "
+
+# GRUB THEMES
+# ultrakill theme
+# wget -P /tmp https://github.com/YouStones/ultrakill-grub-theme/raw/main/install.sh && bash /tmp/install.sh
+
+# poly light dark
+wget -P /tmp https://github.com/shvchk/poly-dark/raw/master/install.sh && bash /tmp/install.sh
+
+# poly light
+#wget -P /tmp https://github.com/shvchk/poly-light/raw/master/install.sh && bash /tmp/install.sh
+
+# anime style
+# git clone https://github.com/13atm01/GRUB-Theme.git && cd GRUB-Theme && sudo sh ./install.sh && sudo update-grub && sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+# cyberpank theme
+# git clone https://github.com/NayamAmarshe/Cyberpunk-GRUB-Theme.git && cd Cyberpunk && sudo ./install.sh
+
+
+
